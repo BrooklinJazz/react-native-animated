82c11b16d0d51747725a999383cc10ba44f03873
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-06-20 18:10:54 -0600

    Pan Handler Example

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import {
   Text,
   ScrollView,
   View,
+  PanResponder,
 } from "react-native";
 import { StyledComponent } from "styled-components";
 import { Fade } from "./Fade";
@@ -20,11 +21,43 @@ const Center = styled.View`
   align-items: center;
 `;
 
-
 export default function App() {
+  let animation = new Animated.ValueXY({ x: 0, y: 0 });
+  let x = 0
+  let y = 0
+  animation.addListener((value) => {
+    x = value.x
+    y = value.y
+  })
+  let _panResponder = PanResponder.create({
+    // onStartShouldSetPanResponder: () => true,
+    onMoveShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event([
+      null,
+      { dx: animation.x, dy: animation.y },
+    ]),
+    onPanResponderGrant: () => {
+      animation.setOffset({x, y});
+      animation.setValue({x: 0, y: 0});
+    },
+    onPanResponderRelease: (e, { vx, vy }) => {
+      Animated.decay(animation, {
+        velocity: { x: vx, y: vy },
+        deceleration: 0.997,
+      }).start();
+    },
+  })
   return (
-      <Center>
-        <Text>placeholder</Text>
-      </Center>
+    <Center style={{backgroundColor: "blue"}}>
+      <Animated.View
+        style={{
+          height: 20,
+          width: 20,
+          backgroundColor: "red",
+          transform: animation.getTranslateTransform(),
+        }}
+        {..._panResponder.panHandlers}
+      />
+    </Center>
   );
 }
